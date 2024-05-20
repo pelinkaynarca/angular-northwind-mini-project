9382e8e0e185245f47d5c0c6811160b73a9058c6
selahattin <selahattindindas@outlook.com>
2024-05-20 17:39:46 +0300

    feat: ceate content foradmin home component.

--- src/app/features/components/admin-home/admin-home.component.ts
+++ src/app/features/components/admin-home/admin-home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryListItem } from '../../categories/models/category-list-item';
+import { ProductListItem } from '../../products/models/product-list-item';
+import { CommonModule } from '@angular/common';
+import { ProductsService } from '../../products/services/products.service';
+import { CategoriesService } from '../../categories/services/categories.service';
 
 @Component({
   selector: 'app-admin-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.css'
 })
-export class AdminHomeComponent {
+export class AdminHomeComponent implements OnInit{
+  categoryList: CategoryListItem[] = [];
+  productList: ProductListItem[] = [];
 
+  constructor(private productsService:ProductsService, private categoriesService:CategoriesService){
+
+  }
+ngOnInit(): void {
+  this.getCategories();
+  this.getProducts();
+}
+getProducts(){
+  this.productsService.getAll().subscribe((productList)=>{
+    this.productList=productList;
+  })
+}
+getCategories(){
+    this.categoriesService.getAll().subscribe((categoryList)=>{
+      this.categoryList=categoryList;
+    })
+}
 }
